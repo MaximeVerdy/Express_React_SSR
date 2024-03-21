010543b5766eff307662326558af3a196486299e
MaximeV <maximeverdy@gmail.com>
2024-03-21 17:25:18 +0100

    more comments added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Transpiling CommonJS modules in Express and React files using Babel
-// Necessary to prevent errors with JS modules syntax 
+// Necessary to prevent errors with JS modules syntax ('import', etc)
 require('@babel/register')({
   presets: ['@babel/preset-env', '@babel/preset-react'],
   extensions: ['.js', '.jsx']
@@ -13,7 +13,7 @@ const React = require('react');
 const {SmallCompo} = require('./src/components/SmallCompo.jsx'); 
 
 
-// server config
+// Server configuration
 const express = require('express');
 const app = express();
 
@@ -21,11 +21,10 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
-  // Render the SmallCompo component to a string
   // In SSR, the initial HTML content needs to be generated on the server and sent to the client. 
   // React components cannot be directly sent over HTTP because they are JavaScript objects. 
   // Instead, they need to be converted into HTML strings.
-  // Until the future hydration, the page will be complete in terms of content but static
+  // NB : Until the future hydration, the page will be complete in terms of content but static
     const componentStr = ReactDOMServer.renderToString(React.createElement(SmallCompo));
 
     res.send(`
